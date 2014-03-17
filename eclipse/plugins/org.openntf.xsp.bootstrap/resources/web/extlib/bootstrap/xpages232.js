2a55e92a5b0ca83292241c305db5f7d4b0ae7472
Mark Leusink <m.leusink@gmail.com>
2014-03-17 10:01:19 +0100

    removed xpages.js JS (code moved to JS file per version)

--- eclipse/plugins/org.openntf.xsp.bootstrap/resources/web/extlib/bootstrap/xpages232.js
+++ eclipse/plugins/org.openntf.xsp.bootstrap/resources/web/extlib/bootstrap/xpages232.js
@@ -2,3 +2,12 @@
   XPages Boostrap specific JavaScript files
   This defines the missing JS code in Bs 2.3
 */
+
+/*
+ * x$ snippet by Mark Roden
+ * http://openntf.org/XSnippets.nsf/snippet.xsp?id=x-jquery-selector-for-xpages
+ */
+function x$(idTag, param){ //Updated 18 Feb 2012
+	idTag=idTag.replace(/:/gi, "\\:")+(param ? param : "");
+	return($("#"+idTag));
+}
--- eclipse/plugins/org.openntf.xsp.bootstrap/resources/web/extlib/bootstrap/xpages300.js
+++ eclipse/plugins/org.openntf.xsp.bootstrap/resources/web/extlib/bootstrap/xpages300.js
@@ -20,3 +20,11 @@ $(document).ready(function() {
 	});
 });
 
+/*
+ * x$ snippet by Mark Roden
+ * http://openntf.org/XSnippets.nsf/snippet.xsp?id=x-jquery-selector-for-xpages
+ */
+function x$(idTag, param){ //Updated 18 Feb 2012
+	idTag=idTag.replace(/:/gi, "\\:")+(param ? param : "");
+	return($("#"+idTag));
+}
